e5221dc0cbe82b552023eaca984498c90b869282
Allan Resende <allanresende@intranet>
2024-09-16 14:23:23 -0300

    Update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Link from "next/link";
+import Image from "next/image";
+import { Suspense } from "react";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+const mockImageUrls = [
+  "https://utfs.io/f/RMDArrATFzJjTeEcLOWp6g4N9W02VwZQxCK7lsifjTo3OGUu",
+  "https://utfs.io/f/RMDArrATFzJjBsxLFCKE35KMQdurwNxyiYIbcPEmJFz6VHka",
+];
+
+const mockImages = mockImageUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
+export default async function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        Hello (Gallery In Progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        <Suspense fallback={<div className='w-48'>Loading...</div>}>
+          {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+            <div key={image.id} className="w-48">
+              <Image src={image.url} alt="Mock Image" width={192} height={192} />
+            </div>
+          ))}
+        </Suspense>
       </div>
     </main>
   );
