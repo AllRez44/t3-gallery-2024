01972628abf84c6a2847a93968c3db979072f06e
Allan Resende <allanresende@intranet>
2024-09-16 15:49:23 -0300

    Add Clerk's Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { Suspense } from "react";
 import { db } from "~/server/db";
@@ -5,19 +6,29 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   const multiImagesCopies = [...images, ...images, ...images, ...images, ...images];
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        <Suspense fallback={<div className='w-48'>Loading...</div>}>
-          {multiImagesCopies.map((image) => (
-            <div key={image.id} className="w-48">
-              <Image src={image.url} alt={image.name} width={192} height={192} />
-            </div>
-          ))}
-        </Suspense>
-      </div>
+      <SignedIn>
+        <div className="flex flex-wrap gap-4 justify-evenly">
+          <Suspense fallback={<div className='w-48'>Loading...</div>}>
+            {multiImagesCopies.map((image) => (
+              <div key={image.id} className="w-48">
+                <Image src={image.url} alt={image.name} width={192} height={192} />
+              </div>
+            ))}
+          </Suspense>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <div className="text-center flex flex-col gap-3 py-3 justify-center">
+          <h1 className="text-4xl font-bold">Welcome to T3 Gallery</h1>
+          <p className="text-lg">Please sign in to view the gallery</p>
+        </div>
+      </SignedOut>
     </main>
   );
 }
