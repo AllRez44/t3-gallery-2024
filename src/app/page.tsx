11770ff3c4bc12d3ea895b81d36b4a17ef32fa98
Allan Resende <allanresende@intranet>
2024-09-17 16:01:15 -0300

    Add file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Images } from "./_components/images";
 import { SignInMessage } from "./_components/signin_message";
+import { UploadButton } from "~/utils/uploadthing";
 
 export const dynamic = "force-dynamic";
 
@@ -8,6 +9,7 @@ export default async function HomePage() {
   return (
     <main className="">
       <SignedIn>
+        <UploadButton endpoint="imageUploader" />
         <Images />
       </SignedIn>
       <SignedOut>
