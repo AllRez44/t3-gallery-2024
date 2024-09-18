aa01280d147b884930a8c27dfad7a74422dd434a
Allan Resende <allanresende@intranet>
2024-09-18 10:27:00 -0300

    Update Images component to get images from queries.ts

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -1,20 +1,25 @@
-import { db } from "~/server/db";
 import Image from "next/image";
 import { Suspense } from "react";
+import { getMyImages } from "~/server/db/queries";
 
 export async function Images() {
-    const images = await db.query.images.findMany({
-        orderBy: (model, { desc }) => desc(model.id),
-    });
-    console.log(images);
+    const images = await getMyImages();
     return <div className="flex flex-wrap gap-4 p-4" >
-            <Suspense fallback={<div className='w-48'>Loading...</div>}>
-                {images.map((image) => (
-                    <div key={image.id} className="w-48 h-48">
-                        <Image src={image.url} className="w-48 h-48" width={192} height={192} alt={image.name} />
-                    </div>
-                ))}
-            </Suspense>
-            {images.length <= 0 && <p className="text-xl w-full text-center p-4">No images uploaded yet</p>}
-        </div>
+        <Suspense fallback={<div className='w-48'>Loading...</div>}>
+            {images.map((image) => (
+                <div key={image.id} className="w-48 h-48">
+                    <Image
+                        src={image.url}
+                        className="w-48 h-48 object-cover"
+                        width={192}
+                        height={192}
+                        alt={image.name}
+                    />
+                </div>
+            ))}
+        </Suspense>
+        {images.length <= 0 &&
+            <p className="text-xl w-full text-center p-4">No images uploaded yet</p>
+        }
+    </div>
 }
