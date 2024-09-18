e9d188e959d4beaabe91791f52df4d9c5895232a
Allan Resende <allanresende@intranet>
2024-09-18 09:22:08 -0300

    Add message when no images are found

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -6,15 +6,15 @@ export async function Images() {
     const images = await db.query.images.findMany({
         orderBy: (model, { desc }) => desc(model.id),
     });
-    return (
-        <div className="flex flex-wrap gap-4 p-4">
+    console.log(images);
+    return <div className="flex flex-wrap gap-4 p-4" >
             <Suspense fallback={<div className='w-48'>Loading...</div>}>
-                {images.map((image, index) => (
-                    <div key={image.id + index} className="w-48 h-48">
+                {images.map((image) => (
+                    <div key={image.id} className="w-48 h-48">
                         <Image src={image.url} className="w-48 h-48" width={192} height={192} alt={image.name} />
                     </div>
                 ))}
             </Suspense>
+            {images.length <= 0 && <p className="text-xl w-full text-center p-4">No images uploaded yet</p>}
         </div>
-    );
 }
