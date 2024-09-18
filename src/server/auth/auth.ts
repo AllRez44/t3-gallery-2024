23a8432563ed314b612c899b233326bd3c75433f
Allan Resende <allanresende@intranet>
2024-09-18 10:26:12 -0300

    Add method to get user from auth

---
+++ src/server/auth/auth.ts
@@ -0,0 +1,8 @@
+import { auth } from "@clerk/nextjs/server";
+
+export function getUser() {
+    "server-only";
+    const user = auth();
+    if (!user.userId) throw new Error("Unauthorized");
+    return user;
+}
